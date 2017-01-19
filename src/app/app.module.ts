00eb71cc2e656fb5823e81a52dc7a6b49bd1499f
Harjot Singh <hsingh@sjcoe.net>
2017-01-18 16:44:45 -0800

    tesla calculator

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { TeslaBatteryModule } from './tesla-battery/tesla-battery.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    TeslaBatteryModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
